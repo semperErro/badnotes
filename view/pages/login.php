8ebe8a777178e0a36bd2d1123f7be02160bd3bb3
niklas <nskf2000@gmail.com>
2021-01-30 06:38:03 +0100

    container and header for login and register

--- view/pages/login.php
+++ view/pages/login.php
@@ -1,26 +1,29 @@
-<form method="post" action="/" class="was-validated">
-    <input type="hidden" name="action" value="login"><br>
+<? include "../view/templates/header.php" ?>
+<div class="container">
+    <form method="post" action="/" class="was-validated">
+        <input type="hidden" name="action" value="login"><br>
 
-    <div class="form-group">
-        <label for="email">E-Mail:</label><br>
-        <input type="email" id="email" name="email" class="form-control" required
-               placeholder="Gebe hier Deine E-Mail ein"><br>
-    </div>
+        <div class="form-group">
+            <label for="email">E-Mail:</label><br>
+            <input type="email" id="email" name="email" class="form-control" required
+                   placeholder="Gebe hier Deine E-Mail ein"><br>
+        </div>
 
-    <div class="form-group">
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" class="form-control" required
-               placeholder="Gebe hier Dein Passwort ein"><br>
-    </div>
+        <div class="form-group">
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" class="form-control" required
+                   placeholder="Gebe hier Dein Passwort ein"><br>
+        </div>
 
-    <div class="form-group form-check">
-        <label for="remember" class="form-check-label">
-            <input type="checkbox" id="remember" name="remember" class="form-check-input" value="Remember">Remember me.
-        </label>
-    </div>
+        <div class="form-group form-check">
+            <label for="remember" class="form-check-label">
+                <input type="checkbox" id="remember" name="remember" class="form-check-input" value="Remember">Remember me.
+            </label>
+        </div>
 
-    <button type="submit" class="btn btn-primary">Einloggen</button>
-    <br><br>
+        <button type="submit" class="btn btn-primary">Einloggen</button>
+        <br><br>
 
-    <p>Noch nicht registriert? <a href="/?page=register">Registriere dich hier.</a></p>
-</form>
+        <p>Noch nicht registriert? <a href="/?page=register">Registriere dich hier.</a></p>
+    </form>
+</div>
--- view/pages/register.php
+++ view/pages/register.php
@@ -1,33 +1,37 @@
-<form method="post" action="/" class="was-validated">
-    <input type="hidden" name="action" value="register"><br>
+<? include "../view/templates/header.php" ?>
 
-    <div class="form-group">
-        <label for="name">Name:</label><br>
-        <input type="text" class="form-control" id="name" name="name" required><br>
-    </div>
+<div class="container">
+    <form method="post" action="/" class="was-validated">
+        <input type="hidden" name="action" value="register"><br>
 
-    <div class="form-group">
-        <label for="email">E-Mail:</label><br>
-        <input type="email" class="form-control" id="email" name="email" required><br>
-    </div>
+        <div class="form-group">
+            <label for="name">Name:</label><br>
+            <input type="text" class="form-control" id="name" name="name" required><br>
+        </div>
 
-    <div class="form-group">
-        <label for="password">Password:</label><br>
-        <input type="password" class="form-control" id="password" name="password" required><br>
-    </div>
+        <div class="form-group">
+            <label for="email">E-Mail:</label><br>
+            <input type="email" class="form-control" id="email" name="email" required><br>
+        </div>
 
-    <div class="form-group">
-        <label for="password">Repeat Password:</label><br>
-        <input type="password" class="form-control" id="password_repeated" name="password_repeated" required><br>
-    </div>
+        <div class="form-group">
+            <label for="password">Password:</label><br>
+            <input type="password" class="form-control" id="password" name="password" required><br>
+        </div>
 
-    <div class="form-group form-check">
-        <input type="checkbox" id="terms" name="terms" value="Terms">
-        <label for="terms"> I accept the terms and conditions.</label><br>
-    </div>
+        <div class="form-group">
+            <label for="password">Repeat Password:</label><br>
+            <input type="password" class="form-control" id="password_repeated" name="password_repeated" required><br>
+        </div>
 
-    <button class="btn btn-primary" type="submit" value="Register">Registrieren</button>
-    <br><br>
+        <div class="form-group form-check">
+            <input type="checkbox" id="terms" name="terms" value="Terms">
+            <label for="terms"> I accept the terms and conditions.</label><br>
+        </div>
 
-    <p>Schon registriert? <a href="/?page=login">Melde dich hier an.</a></p>
-</form>
+        <button class="btn btn-primary" type="submit" value="Register">Registrieren</button>
+        <br><br>
+
+        <p>Schon registriert? <a href="/?page=login">Melde dich hier an.</a></p>
+    </form>
+</div>
